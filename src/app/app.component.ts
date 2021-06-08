d4277ddbb218801cc480ac9d63e4789be7b31f0d
Gagik Eghiazaryan <egagik69@gmail.com>
2021-06-09 00:37:27 +0400

    ramka

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  imageObject = [{
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
-}, {
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg'
-}, {
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/4.jpg',
-},{
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/7.jpg',
-}, {
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg'
-}, {
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/2.jpg',
-
-}];
+    imageObject = [{
+        thumbImage: './assets/ramka_img/1_thumb.jpg', 
+    }, {
+        thumbImage: './assets/ramka_img/2_thumb.jpg', 
+    }, {
+        thumbImage: './assets/ramka_img/3_thumb.jpg',
+    }, {
+        thumbImage: './assets/ramka_img/5_thumb.jpg',
+    }, {
+        thumbImage: './assets/ramka_img/6_thumb.jpg',
+    }, {
+        thumbImage: './assets/ramka_img/9_thumb.jpg',
+    },{
+        thumbImage: './assets/ramka_img/11_thumb.jpg'
+    }];
 }
