6d659320513c734ca5cacac943ba9045ae6b2acf
GagoEgh <egagik69@gmail.com>
2021-06-09 17:33:25 +0400

    bg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FramesServService } from './frames-serv.service';
+
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(public frames:FramesServService){}
+    ngOnInit(): void {
+        this.imageClick(this.frames.index);
+    }
 
-    imageObject = [{
-        thumbImage: './assets/ramka_img/1_thumb.jpg', 
-    }, {
-        thumbImage: './assets/ramka_img/2_thumb.jpg', 
-    }, {
-        thumbImage: './assets/ramka_img/3_thumb.jpg',
-    }, {
-        thumbImage: './assets/ramka_img/5_thumb.jpg',
-    }, {
-        thumbImage: './assets/ramka_img/6_thumb.jpg',
-    }, {
-        thumbImage: './assets/ramka_img/9_thumb.jpg',
-    },{
-        thumbImage: './assets/ramka_img/11_thumb.jpg'
-    }];
+    imageClick($event:number){
+        this.frames.index = $event+1;
+        this.frames.frame = this.frames.frames.find(item=>item.id === this.frames.index);
+      //  console.log(this.frames.imageObject[$event])
+      this.frames.imageObject[$event]
+    }
+
+    changeBg(){
+        console.log()
+    }
 }
